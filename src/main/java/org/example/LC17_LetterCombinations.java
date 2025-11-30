3db18737033eaa40272d0805172bf9ea8575a33c
umangparekh <umangparekh.in@gmail.com>
2025-11-30 12:32:50 +0100

    LeetCode Practices - LC17 version1

--- src/main/java/org/example/LC17_LetterCombinations.java
+++ src/main/java/org/example/LC17_LetterCombinations.java
@@ -3,34 +3,45 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *  LC17 : Letter Combinations of a Phone Number
+ *  Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+ *
+ * A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ *
+ * Example 1:
+ *
+ * Input: digits = "23"
+ * Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ *
+ * Example 2:
+ *
+ * Input: digits = "2"
+ * Output: ["a","b","c"]
+ *
+ * Constraints:
+ *
+ * 1 <= digits.length <= 4
+ * digits[i] is a digit in the range ['2', '9'].
+ *
+ *
+ *
+ */
 public class LC17_LetterCombinations {
     static StringBuilder sb = new StringBuilder();
     static List<String> res = new ArrayList<>();
 
     public static void main(String[] args) {
-        String input ="234";
+        String input = "234";
         System.out.println(" input number >> " + input + " || Letter Combinations >> " + letterCombinations(input));
 
 
     }
 
-    public static void backtrack(String[] numString, String digits, int len) {
-        if (len == digits.length()) {
-            res.add(sb.toString());
-            return;
-        }
-        String cur = numString[digits.charAt(len) - '0'];
-        for (int i = 0; i < cur.length(); i++) {
-            sb.append(cur.charAt(i));
-            backtrack(numString, digits, len + 1);
-            sb.deleteCharAt(sb.length() - 1);
-        }
-    }
-
     public static List<String> letterCombinations(String digits) {
         List<String> results = new ArrayList<>();
         // handle edge case of empty string
-        if (digits == null || digits.length() == 0) {
+        if (digits.isEmpty()) {
             return results;
         }
         // start with adding emptry string
@@ -39,20 +50,20 @@ public class LC17_LetterCombinations {
         //Mapping of digits to corresponding letters
         // index 0 represets digit '2' index 1 represents digit '3'
         String[] digitToLetters = new String[]{
-                "", "", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
+                "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
         };
 
-        for(char digit: digits.toCharArray()){
-            System.out.println(" index >> "+(digit - '2'));
-            String letters = digitToLetters[digit];
+        for (char digit : digits.toCharArray()) {
+            System.out.println(" index >> " + (digit - '2'));
+            String letters = digitToLetters[digit - '2'];
 
             // initialise tempList
             List<String> tempCombinations = new ArrayList<>();
 
             // for each combinations in result
-            for(String existingCombinations : results){
-                for(char letter:letters.toCharArray()){
-                    tempCombinations.add(existingCombinations+letter);
+            for (String existingCombinations : results) {
+                for (char letter : letters.toCharArray()) {
+                    tempCombinations.add(existingCombinations + letter);
                 }
             }
             results = tempCombinations;
@@ -60,4 +71,5 @@ public class LC17_LetterCombinations {
 
         return results;
     }
+
 }
