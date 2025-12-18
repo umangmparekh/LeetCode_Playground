09e57eca26535cfec2a2fc134fdb69e09074ce05
umangparekh <umangparekh.in@gmail.com>
2025-12-18 23:55:09 +0100

    LeetCode Practices - LC71_SimplifyPath

---
+++ src/main/java/org/example/LC71_SimplifyPath.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.Stack;
+
+public class LC71_SimplifyPath {
+
+    public static void main(String[] args) {
+
+        String path = "/home/user/Documents/../Pictures";
+        System.out.println(" LC71_SimplifyPath  >> " + simplifyPath(path));
+    }
+
+    public static String simplifyPath(String path){
+        Stack<String> stack = new Stack<>();
+        String[] components = path.split("/");
+
+        for(String directory : components){
+            if(directory.equals(".") || directory.isEmpty()){
+                continue;
+            }
+            else if(directory.equals("..")){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            } else {
+                stack.add(directory);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(String dir : stack){
+            result.append("/");
+            result.append(dir);
+        }
+
+        return result.length() > 0 ? result.toString() : "/";
+
+    }
+}
